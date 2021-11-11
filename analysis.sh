4096c512fc6998815559cd805f4efe3f241424ec
peakchen90 <1558267774@qq.com>
2021-11-11 17:44:11 +0800

    chore: 添加图形分析工具脚本

--- analysis.sh
+++ analysis.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # TODO: 需依赖工具 http://www.graphviz.org/
+# 首先需要执行 `cargo run` 命令生成 .debug.ll 文件后才能分析
+# 可以通过追加 `-a` 参数自动打开 svg 文件, Usage: `./analysis.sh -o`
 
 rm -rf .temp/.*.dot
 rm -rf .temp/.*.dot.svg
@@ -16,4 +18,7 @@ for file in `find .temp -name "*.dot"`
 do
   dot $file -Tsvg -o $file.svg
   echo "Generated svg file: $file.svg"
+  if [[ $1 == "-o" ]];  then
+     open $file.svg
+  fi
 done
